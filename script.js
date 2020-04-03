a5a0ae4fe3cdcffcc990975f97336c0dc3d03dbe
Caitlin Wang <caitlinwang97@gmail.com>
2020-04-03 18:19:38 -0400

    Implement dark mode toggle

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Toggle light/dark mode
+const toggle = document.querySelector('#theme-toggle');
+const toggleLabel = document.querySelector('.theme-toggle__label');
+
+toggle.addEventListener('change', (e) => {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    toggleLabel.innerHTML = "Turn on the lights";
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    toggleLabel.innerHTML = "Turn off the lights";
+  }
+})
+
+// Table of Contents
 const options = {
   root: null,
   rootMargin: '0px',
@@ -9,9 +24,8 @@ let selected = document.querySelector('.selected');
 
 let callback = (entries) => {
   entries.forEach(entry => {
-    console.log(entry);
     if (entry.isIntersecting) {
-      let sectionId = entry.target.id;
+      const sectionId = entry.target.id;
 
       for (let i = 0; i < tocLinks.length; i++) {
         const link = tocLinks[i];
